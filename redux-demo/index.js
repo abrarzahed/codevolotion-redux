e33f0a793a2709876c9d323e783dcf159c3d4a65
abrarzahed <abrarzahed6986@gmail.com>
2023-01-02 14:18:20 +0600

    configured

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,1 +1,40 @@
-console.log("Hello World");
+const { createStore } = require("redux");
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+const orderCake = () => {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+};
+
+const initialState = {
+  numOfCake: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCake: state.numOfCake - action.quantity,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+console.log("initial state: ", store.getState());
+const storeUnsubscribe = store.subscribe(() => {
+  console.log("initial state: ", store.getState());
+});
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+storeUnsubscribe();
